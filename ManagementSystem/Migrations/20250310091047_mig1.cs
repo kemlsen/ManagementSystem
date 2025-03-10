3f5cc4206fac9ed61cbdaae30d5927df9e2e65b7
Kemal ŞEN <kemal_sen352@hotmail.com>
2025-03-10 12:14:17 +0300

    added migration

---
+++ ManagementSystem/Migrations/20250310091047_mig1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Services",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Services", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordSalt = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    PasswordHash = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    UserType = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Appointments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AppointmentDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    ServiceId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Appointments_Services_ServiceId",
+                        column: x => x.ServiceId,
+                        principalTable: "Services",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Appointments_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "FirstName", "LastName", "PasswordHash", "PasswordSalt", "UserName", "UserType" },
+                values: new object[,]
+                {
+                    { 1, "Dincer", "Teknoloji", new byte[] { 11, 54, 62, 240, 159, 165, 184, 150, 168, 114, 83, 20, 142, 97, 71, 182, 100, 171, 169, 16, 205, 16, 7, 251, 84, 237, 243, 76, 117, 235, 46, 226, 17, 88, 82, 212, 112, 64, 155, 240, 63, 69, 146, 217, 32, 194, 35, 2, 119, 182, 34, 135, 143, 33, 10, 184, 24, 62, 131, 182, 235, 177, 81, 99 }, new byte[] { 132, 225, 102, 92, 223, 144, 89, 34, 201, 70, 254, 48, 247, 32, 115, 109, 186, 132, 232, 94, 124, 40, 202, 53, 197, 146, 231, 211, 126, 72, 179, 96, 30, 85, 82, 7, 42, 213, 204, 142, 121, 132, 24, 42, 127, 152, 131, 128, 53, 168, 88, 153, 13, 116, 182, 8, 69, 249, 181, 197, 197, 223, 187, 194, 189, 122, 178, 12, 124, 250, 166, 210, 204, 54, 100, 124, 39, 59, 145, 189, 119, 233, 8, 93, 113, 189, 161, 67, 185, 203, 57, 29, 136, 91, 197, 234, 14, 199, 17, 192, 243, 157, 71, 186, 238, 174, 237, 63, 85, 78, 62, 67, 177, 249, 107, 12, 121, 186, 56, 203, 177, 166, 230, 1, 163, 75, 223, 80 }, "admindincer", 2 },
+                    { 2, "Kemal", "Sen", new byte[] { 11, 54, 62, 240, 159, 165, 184, 150, 168, 114, 83, 20, 142, 97, 71, 182, 100, 171, 169, 16, 205, 16, 7, 251, 84, 237, 243, 76, 117, 235, 46, 226, 17, 88, 82, 212, 112, 64, 155, 240, 63, 69, 146, 217, 32, 194, 35, 2, 119, 182, 34, 135, 143, 33, 10, 184, 24, 62, 131, 182, 235, 177, 81, 99 }, new byte[] { 132, 225, 102, 92, 223, 144, 89, 34, 201, 70, 254, 48, 247, 32, 115, 109, 186, 132, 232, 94, 124, 40, 202, 53, 197, 146, 231, 211, 126, 72, 179, 96, 30, 85, 82, 7, 42, 213, 204, 142, 121, 132, 24, 42, 127, 152, 131, 128, 53, 168, 88, 153, 13, 116, 182, 8, 69, 249, 181, 197, 197, 223, 187, 194, 189, 122, 178, 12, 124, 250, 166, 210, 204, 54, 100, 124, 39, 59, 145, 189, 119, 233, 8, 93, 113, 189, 161, 67, 185, 203, 57, 29, 136, 91, 197, 234, 14, 199, 17, 192, 243, 157, 71, 186, 238, 174, 237, 63, 85, 78, 62, 67, 177, 249, 107, 12, 121, 186, 56, 203, 177, 166, 230, 1, 163, 75, 223, 80 }, "userkemal", 1 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_ServiceId",
+                table: "Appointments",
+                column: "ServiceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_UserId",
+                table: "Appointments",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Appointments");
+
+            migrationBuilder.DropTable(
+                name: "Services");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- ManagementSystem/Models/ManagementContext.cs
+++ ManagementSystem/Models/ManagementContext.cs
@@ -1,5 +1,7 @@
 ﻿using ManagementSystem.Models.Entities;
+using ManagementSystem.Models.Helpers;
 using Microsoft.EntityFrameworkCore;
+using System.Text;
 
 namespace ManagementSystem.Models
 {
@@ -17,44 +19,25 @@ namespace ManagementSystem.Models
         {
             base.OnModelCreating(modelBuilder);
 
-
-            modelBuilder.Entity<Service>().HasData(
-                new Service
-                {
-                    Id = 1,
-                    Name = "Egzoz Gazı Ölçümü"
-                },
-                new Service
-                {
-                    Id = 2,
-                    Name = "Fren Testi"
-                },
-                new Service
-                {
-                    Id = 3,
-                    Name = "Far Ayarı"
-                }
-            );
-
             modelBuilder.Entity<User>().HasData(
                 new User
                 {
                     Id = 1,
-                    FirstName = "Kemal",
-                    LastName = "Sen",
-                    UserName = "kemlsen",
-                    PasswordSalt = Convert.FromBase64String("3ttkFQ6rqgOkFCyqEPr6h68TxHAT90I4ztzS5Pl2m10="),
-                    PasswordHash = Convert.FromBase64String("uTjdK35Y3Ut9pTrDdI1gx6JgpJ0E0hx0tOupoc4z9J3TrHZxxusS8JHDWfF9Eu3qyd9pcUeZOhlRgdQ2n60OrQ=="),
+                    FirstName = "Dincer",
+                    LastName = "Teknoloji",
+                    UserName = "admindincer",
+                    PasswordSalt = new byte[] { 132, 225, 102, 92, 223, 144, 89, 34, 201, 70, 254, 48, 247, 32, 115, 109, 186, 132, 232, 94, 124, 40, 202, 53, 197, 146, 231, 211, 126, 72, 179, 96, 30, 85, 82, 7, 42, 213, 204, 142, 121, 132, 24, 42, 127, 152, 131, 128, 53, 168, 88, 153, 13, 116, 182, 8, 69, 249, 181, 197, 197, 223, 187, 194, 189, 122, 178, 12, 124, 250, 166, 210, 204, 54, 100, 124, 39, 59, 145, 189, 119, 233, 8, 93, 113, 189, 161, 67, 185, 203, 57, 29, 136, 91, 197, 234, 14, 199, 17, 192, 243, 157, 71, 186, 238, 174, 237, 63, 85, 78, 62, 67, 177, 249, 107, 12, 121, 186, 56, 203, 177, 166, 230, 1, 163, 75, 223, 80 }, 
+                    PasswordHash = new byte[] { 11, 54, 62, 240, 159, 165, 184, 150, 168, 114, 83, 20, 142, 97, 71, 182, 100, 171, 169, 16, 205, 16, 7, 251, 84, 237, 243, 76, 117, 235, 46, 226, 17, 88, 82, 212, 112, 64, 155, 240, 63, 69, 146, 217, 32, 194, 35, 2, 119, 182, 34, 135, 143, 33, 10, 184, 24, 62, 131, 182, 235, 177, 81, 99 },
                     UserType = UserType.Admin
                 },
                 new User
                 {
                     Id = 2,
-                    FirstName = "Ahmet",
-                    LastName = "Hassan",
-                    UserName = "ahassan",
-                    PasswordSalt = Convert.FromBase64String("3ttkFQ6rqgOkFCyqEPr6h68TxHAT90I4ztzS5Pl2m10="),
-                    PasswordHash = Convert.FromBase64String("uTjdK35Y3Ut9pTrDdI1gx6JgpJ0E0hx0tOupoc4z9J3TrHZxxusS8JHDWfF9Eu3qyd9pcUeZOhlRgdQ2n60OrQ=="),
+                    FirstName = "Kemal",
+                    LastName = "Sen",
+                    UserName = "userkemal",
+                    PasswordSalt = new byte[] { 132, 225, 102, 92, 223, 144, 89, 34, 201, 70, 254, 48, 247, 32, 115, 109, 186, 132, 232, 94, 124, 40, 202, 53, 197, 146, 231, 211, 126, 72, 179, 96, 30, 85, 82, 7, 42, 213, 204, 142, 121, 132, 24, 42, 127, 152, 131, 128, 53, 168, 88, 153, 13, 116, 182, 8, 69, 249, 181, 197, 197, 223, 187, 194, 189, 122, 178, 12, 124, 250, 166, 210, 204, 54, 100, 124, 39, 59, 145, 189, 119, 233, 8, 93, 113, 189, 161, 67, 185, 203, 57, 29, 136, 91, 197, 234, 14, 199, 17, 192, 243, 157, 71, 186, 238, 174, 237, 63, 85, 78, 62, 67, 177, 249, 107, 12, 121, 186, 56, 203, 177, 166, 230, 1, 163, 75, 223, 80 }, 
+                    PasswordHash = new byte[] { 11, 54, 62, 240, 159, 165, 184, 150, 168, 114, 83, 20, 142, 97, 71, 182, 100, 171, 169, 16, 205, 16, 7, 251, 84, 237, 243, 76, 117, 235, 46, 226, 17, 88, 82, 212, 112, 64, 155, 240, 63, 69, 146, 217, 32, 194, 35, 2, 119, 182, 34, 135, 143, 33, 10, 184, 24, 62, 131, 182, 235, 177, 81, 99 },
                     UserType = UserType.User
                 }
             );
